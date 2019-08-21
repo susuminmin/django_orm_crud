2741e5ddaf3fb5ca5d6c68e0dc0b66928bff6b49
susuminmin <mytoutbon@gmail.com>
2019-08-21 17:19:44 +0900

    Article 모델 만들었습니다.
     - Articles 어플리케이션 만들었습니다.
     - Article 모델 만들고 DB 반영했습니다.

--- crud/settings.py
+++ crud/settings.py
@@ -33,7 +33,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # local apps
     'articles',
-    
+
+    #
+
+    # 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
