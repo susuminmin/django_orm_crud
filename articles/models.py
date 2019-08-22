ee843e88faf8c13c1bcd50fcec2ba93feabc6a90
susuminmin <mytoutbon@gmail.com>
2019-08-22 12:09:23 +0900

    Create README.md
     - Learn CRUD Operation with Django ORM

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# 순차적으로 3단계!! 잔고의 db에 적용하는 방법은?
+# 0. model 을 작성한다.
+# 1. python manage.py makemigrations => django 에게 model 작성했음을 알림
+# 2. python manage.py migrate => django 에게 실제 DB에 작성하라고 명령
+
+
 class Article(models.Model):
-    # id(pk)는 기본적으로 처음 테이블 생성시 자동으로 만들어진다. 
+    # id(pk)는 기본적으로 처음 테이블 생성시 자동으로 만들어진다.
     # id = models.AutoFiled(primary_key=True)
 
-    # CharField 에서는 max_length가 필수 인자다. 
-    title = models.CharField(max_length=20) # 클래스 변수 (DB의 필드)
-    content = models.TextField() # 클래스 변수 (DB의 필드)
-    created_at = models.DateTimeField(auto_now_add=True) # 자동으로 지금 추가됐을 때
-    
+    # 모든 필드는 기본적으로 NOT NULL => 비어있으면 안 된다.
+
+    # CharField 에서는 max_length가 필수 인자다.
+    # 클래스 변수 (DB의 필드), max_length 인자 필수
+    title = models.CharField(max_length=20)
+    content = models.TextField()  # 클래스 변수 (DB의 필드)
+    created_at = models.DateTimeField(auto_now_add=True)  # 자동으로 지금 추가됐을 때
+    updated_at = models.DateTimeField(auto_now=True)  # 언제든지
+
+    def __str__(self):
+        return f'{self.id}번 글 - {self.title}: {self.content}'
+    # 모델 변경 X 기 때문에 makemigration이나 migrate은 불필요
+    # 
